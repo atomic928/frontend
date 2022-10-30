9aea22b63edf099684d9eac7ed7ae9a24320d86b
atomic928 <b1020055@fun.ac.jp>
2022-10-30 16:14:01 +0900

    テーマを動的に変える

--- Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/notifications/NotificationsFragment.kt
+++ Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/notifications/NotificationsFragment.kt
@@ -1,13 +1,18 @@
 package com.example.hackathon2022.ui.notifications
 
+import android.app.UiModeManager.MODE_NIGHT_NO
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.hackathon2022.databinding.FragmentNotificationsBinding
+import com.jthemedetecor.OsThemeDetector
 
 class NotificationsFragment : Fragment() {
 
@@ -31,6 +36,15 @@ class NotificationsFragment : Fragment() {
         val textView: TextView = binding.textNotifications
         notificationsViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
+        }
+
+        val buttonDarkTheme: Button = binding.btDarkTheme
+        val buttonLightTheme: Button = binding.btLightTheme
+        buttonDarkTheme.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        }
+        buttonLightTheme.setOnClickListener {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         }
         return root
     }
