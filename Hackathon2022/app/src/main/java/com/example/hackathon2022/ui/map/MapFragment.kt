99acb7f9a0251f5fcccb8716fa40b5bba5471985
atomic928 <b1020055@fun.ac.jp>
2022-11-30 21:42:04 +0900

    タップしたリストの日時を受け取れるようにした

--- Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/map/MapFragment.kt
+++ Hackathon2022/app/src/main/java/com/example/hackathon2022/ui/map/MapFragment.kt
@@ -1,12 +1,15 @@
 package com.example.hackathon2022.ui.map
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.fragment.app.activityViewModels
+import androidx.fragment.app.setFragmentResult
+import androidx.fragment.app.setFragmentResultListener
 import com.example.hackathon2022.SensorViewModel
 import com.example.hackathon2022.databinding.FragmentMapBinding
 
@@ -27,6 +30,11 @@ class MapFragment : Fragment() {
 
         _binding = FragmentMapBinding.inflate(inflater, container, false)
         val root: View = binding.root
+
+        setFragmentResultListener("stringData") { key, bundle ->
+            val result = bundle.getString("itemString")
+            Log.d("resultTest", result.toString())
+        }
 
         val ivMap: ImageView = binding.ivMap
         viewModel.map.observe(viewLifecycleOwner) {
